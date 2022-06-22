bee91f29c42e8cc44b5ca38e7157de0c4afad8b0
KStefLarsson <mille83.sl@gmail.com>
2022-06-22 23:18:03 +0200

    Created Interface  and service for ILotteryService

---
+++ WinningNumber/Services/ILotteryServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WinningNumber.Services
+{
+    public interface ILotteryServices
+    {
+        string GetWinningNumber();
+    }
+}
---
+++ WinningNumber/Services/LotteryService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WinningNumber.Services
+{
+    public class LotteryService : ILotteryServices
+    {
+        private static readonly Random rnd = new();
+        public string GetWinningNumber()
+        {
+            string number = " ";
+            for (int i = 0; i < 7; i++)
+            {
+                number += rnd.Next(0, 10).ToString();
+            }
+            return number;
+        }
+    }
+}
