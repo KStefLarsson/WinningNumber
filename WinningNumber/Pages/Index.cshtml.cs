1c53905101115a6bfdfa9c602cc0bf5023246968
KStefLarsson <mille83.sl@gmail.com>
2022-06-22 23:19:00 +0200

    Creates and displays thw winning number side

---
+++ WinningNumber/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WinningNumber.Services;
+
+namespace WinningNumber.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly ILotteryServices _lotteryService;
+
+        public IndexModel(ILogger<IndexModel> logger, ILotteryServices lotteryService)
+        {
+            _logger = logger;
+            _lotteryService = lotteryService;
+        }
+
+        public string WinningNumber { get; set; }
+
+        public void OnGet()
+        {
+            WinningNumber = _lotteryService.GetWinningNumber();
+
+        }
+    }
+}
